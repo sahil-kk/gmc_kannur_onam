db74fa560bc6521a5117564995ff67d189cb7243
sahil-kk <oprsahilkk@gmail.com>
2026-08-26 22:53:56 +0530

    Update copy logic to use phone number instead of UPI ID

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -110,7 +110,7 @@ function EntryForm() {
           <button 
             type="button"
             onClick={() => {
-              navigator.clipboard.writeText("BHARATPE09Z9J1F8L2M84938@yesbankltd");
+              navigator.clipboard.writeText("9744000477");
               setIsCopied(true);
               setTimeout(() => setIsCopied(false), 3000);
             }}
@@ -124,13 +124,13 @@ function EntryForm() {
             ) : (
               <>
                 <svg className="w-5 h-5 opacity-90" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
-                Copy UPI ID for GPay
+                Copy Phone Number
               </>
             )}
           </button>
 
           <div className="text-sm font-medium text-[#ffd878] bg-[#ffd878]/10 py-2 px-4 rounded-full inline-block border border-[#ffd878]/20 break-all mx-2">
-            UPI: BHARATPE09Z9J1F8L2M84938@yesbankltd
+            Number: 9744000477
           </div>
         </div>
 
